f4b117f93633c2411fccf15c2e093a99cc7efc85
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-14 19:35:46 +0530

    Time: 3 ms (27.55%), Space: 7.8 MB (49.45%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s;
+        int start=0,end=address.size()-1;
+        while(start<=end){
+            if(address[start]=='.'){
+               s=s+"[.]";
+                
+            }
+            else s.push_back(address[start]);
+            start++;
+        }
+        return s;
+        
+    }
+};
