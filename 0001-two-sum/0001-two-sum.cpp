6ee208db9976964529df27af39e2936bbf76f1a8
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-20 22:45:30 +0530

    Time: 2 ms (75.9%), Space: 14.7 MB (50.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,16 +6,17 @@ public:
         vector<pair<int,int>> v;
         for(int i=0;i<nums.size();i++){
 
-        v.push_back({nums[i],i});
+        v.push_back({nums[i],i});// nums[i] ko jo index indx assign kr diya vo sorting karne
+                                 // ke bad bhi uske sath rahega
         }
         
         sort(v.begin(),v.end());
         int start=0,end=v.size()-1;
         while(start<end){
-           int sum= v[start].first+v[end].first;
+           int sum= v[start].first+v[end].first;// ye v ke indx ki value ko add krege 
             if(sum==target){
                 
-                return {v[start].second,v[end].second};
+                return {v[start].second,v[end].second};// original indx ko print karega 
                 
             }
             else if(sum<target){
