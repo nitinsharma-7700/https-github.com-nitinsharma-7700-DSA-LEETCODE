b5794e336a668b20db4f12f591d44e60af214d63
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-20 21:50:14 +0530

    Time: 0 ms (100%), Space: 13.9 MB (99.94%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,11 +7,11 @@ public:
         int start=0,end=nums.size()-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]>=nums[0]){
+            if(nums[mid]>=nums[0]){ // yha right side wale sorted elements hmesa nums[0] se chote hoge isilie min ellement wahi milega 
                 start=mid+1;
             }
             else{
-                ans=nums[mid];
+                ans=nums[mid];// kya pta yahi minimum element ho to phele hi ans me store kr lo
                 end=mid-1;
             }
 
