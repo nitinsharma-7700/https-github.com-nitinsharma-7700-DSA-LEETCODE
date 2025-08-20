d67685341780cfea957c0dccb3af4f6154276131
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-20 18:14:49 +0530

    Time: 3 ms (19%), Space: 8.6 MB (14.53%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int n) {
+        int ans=0, start=1,end=n;
+        while(start<=end){
+            long mid=start+(end-start)/2;
+            if(mid*mid==n){
+                ans=mid;
+                break;
+            }
+            else if(mid*mid<n){
+                start=mid+1;
+                ans=mid;
+
+
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
