f13f8f22ab744aebb2e8070c21ee3914505ebff2
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-21 17:10:57 +0530

    Time: 0 ms (100%), Space: 9.2 MB (61.52%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -4,18 +4,19 @@ public:
         vector<int>lower(26,0),upper(26,0);
         for(int i=0;i<s.size();i++){
             if(s[i]>='a'){
-                lower[s[i]-'a']++;
+                lower[s[i]-'a']++;// no of uppercase and char are calculating 
             }
             else upper[s[i]-'A']++;
         }
         int count=0;
         bool odd=0;
         for(int i=0;i<26;i++){
-            if(lower[i]%2==0){
+            if(lower[i]%2==0){ // agar no. even ha to unhe count me ese hi add kar de 
                 count+=lower[i];
             }
-            else {count+=lower[i]-1;
-            odd=1;
+            else {count+=lower[i]-1;// nahi to ek kam karke add krde
+            odd=1;// agar koi odd ya single h usko add kar jo centre ko cover kre
+                  // eg. aacbcaa odd =1 b ke liye kiya ha 
             }
             if(upper[i]%2==0){
                 count+=upper[i];
