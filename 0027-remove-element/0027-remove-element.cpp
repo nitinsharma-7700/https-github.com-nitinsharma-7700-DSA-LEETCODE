28d83b0f95c6680cd57c3f627c661a6a72ae622e
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-25 13:51:57 +0530

    Time: 0 ms (100%), Space: 11.8 MB (7.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int k = 0; // pointer for the "new array"
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {   // keep only non-val elements
-                nums[k] = nums[i];
+                nums[k]=nums[i];
                 k++;
             }
         }
