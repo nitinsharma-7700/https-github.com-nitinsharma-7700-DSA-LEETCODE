73777ef8bf93c60c0adb9e464e77a97a6f90ea56
NITIN SHARMA <panditnitin5185@gmail.com>
2025-08-26 19:20:53 +0530

    Time: 0 ms (100%), Space: 12.8 MB (27.6%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void subseq(vector<int>& nums,int index,int n,vector<vector<int>> &ans, vector<int>&temp)
+    {
+        if(index==n){
+            ans.push_back(temp);
+            return;
+        }
+        subseq(nums,index+1,n,ans,temp);//if not include
+        temp.push_back(nums[index]);// if include
+        subseq(nums,index+1,n,ans,temp);
+        temp.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>temp;
+        subseq(nums,0,nums.size(),ans,temp);
+        return ans;
+    }
+};
