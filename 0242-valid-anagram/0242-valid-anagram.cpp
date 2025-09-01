411de4fced4490f854bd79833a7114f22933825e
NITIN SHARMA <panditnitin5185@gmail.com>
2025-09-01 19:36:22 +0530

    Time: 0 ms (100%), Space: 9.5 MB (91.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,15 +4,15 @@ public:
         if(s.size()!=t.size()){
             return 0;
         }
-        vector<int> count_1(26,0);
+        vector<int> count(26,0);
         vector<int> count_2(26,0);
         for(int i=0;i<s.size();i++){
-            count_1[s[i]-'a']++;
-             count_2[t[i]-'a']++;
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
         }
        
         for(int i=0;i<26;i++){
-            if(count_1[i]!=count_2[i]){
+            if(count[i]!=0){
                 return 0;
             }
         }
