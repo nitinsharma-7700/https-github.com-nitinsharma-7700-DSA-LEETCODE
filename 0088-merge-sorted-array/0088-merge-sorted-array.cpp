559301faf9401177d183963621e6c32a3022ed8b
NITIN SHARMA <panditnitin5185@gmail.com>
2025-09-11 22:38:33 +0530

    Time: 2 ms (3.69%), Space: 12.2 MB (70.59%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;      // last element in nums1's valid part
+        int j = n - 1;      // last element in nums2
+        int k = m + n - 1;  // last position in nums1
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // if nums2 still has elements left
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
