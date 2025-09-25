9230635c7cbd6b135ddef99bf0354e4265304d49
NITIN SHARMA <panditnitin5185@gmail.com>
2025-09-25 17:19:04 +0530

    Time: 16 ms (83.22%), Space: 81.2 MB (82.08%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>right(n);
-        vector<int>left(n);
-        stack<int> st;
-        // for nsr
-        for(int i=0;i<n;i++){
-            while(!st.empty()&& heights[st.top()]>heights[i]){
-                right[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        while(!st.empty()){
-            right[st.top()]=n;
-            st.pop();
-        }
-        // for nsl
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&& heights[st.top()]>heights[i]){
-                left[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        while(!st.empty()){
-            left[st.top()]=-1;
-            st.pop();
-        }
         int ans=0;
+        int n=heights.size();
+        stack<int> st;
         for(int i=0;i<n;i++){
-            ans=max(ans,heights[i]*(right[i]-left[i]-1));
+            while(!st.empty()&&heights[st.top()]>heights[i]){
+                int index=st.top();
+                st.pop();
+                if(!st.empty()){
+                    ans=max(ans,heights[index]*(i-st.top()-1));
+                }
+                else{
+                    ans=max(ans,heights[index]*(i));
+                }
+
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int index=st.top();
+            st.pop();
+            if(!st.empty()){
+                ans=max(ans,heights[index]*(n-st.top()-1));
+            }
+            else{
+                ans=max(ans,heights[index]*(n));
+            }
         }
         return ans;
-    
     }
 };
