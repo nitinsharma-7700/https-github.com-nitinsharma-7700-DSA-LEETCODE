283ea9f151e03795f982a34d6980f3eef16a2061
NITIN SHARMA <panditnitin5185@gmail.com>
2026-03-21 18:20:57 +0530

    Time: 3 ms (67.71%), Space: 14.8 MB (35.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,33 @@
-#include<vector>
-#include<algorithm>
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>> v;
-        for(int i=0;i<nums.size();i++){
+        vector<int>ans;
+        vector<pair<int,int>>v;
 
-        v.push_back({nums[i],i});// nums[i] ko jo index indx assign kr diya vo sorting karne
-                                 // ke bad bhi uske sath rahega
+        for(int i=0;i<nums.size();i++){
+          v.push_back({nums[i],i});
         }
-        
         sort(v.begin(),v.end());
-        int start=0,end=v.size()-1;
+
+        int start=0,end=nums.size()-1;
         while(start<end){
-           int sum= v[start].first+v[end].first;// ye v ke indx ki value ko add krege 
-            if(sum==target){
-                
-                return {v[start].second,v[end].second};// original indx ko print karega 
-                
-            }
-            else if(sum<target){
-                start++;
-            }
-            else {
-                end--;
-            }
+           int sum=v[start].first+v[end].first;
+           if(sum==target)
+           return {v[start].second,v[end].second};
+
+           else if(sum>target)
+           end--;
+
+           else
+           start++;
+           }
+
+           return {};
+
         }
-        return {};
-    }
+
+
+
+    
 };
